6edd475762d231269d711808d8c4fe7a03094637
cait <caitlin@madetech.com>
2021-02-25 16:16:58 +0000

    Combine Image and Captioned components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import Image from "./components/Image/Image";
-import Captioned from "./components/Captioned/Captioned";
 import "./App.css";
 
 function Todo({ todo, index, completeTodo, removeTodo }) {
@@ -85,10 +84,9 @@ function App() {
         ))}
         <TodoForm addTodo={addTodo} />
       </div>
-      <Image />
-      <Captioned src="https://cdn.mos.cms.futurecdn.net/2Gwau7TtiHM5PdsjFeaxnm-320-80.jpg">
-        <div className="">This uses props.children</div>
-      </Captioned>
+      <Image src="https://cdn.mos.cms.futurecdn.net/2Gwau7TtiHM5PdsjFeaxnm-320-80.jpg" alt="Red Panda">
+        <div className="">This uses props.children and takes no state</div>
+      </Image>
     </div>
   );
 }
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,13 +1,9 @@
-import React from "react";
 import classes from "./Image.module.css";
 
-const Image = () => (
+const Image = (props) => (
   <div className={classes.frame}>
-    <img
-      className={classes.image}
-      src="https://cdn.mos.cms.futurecdn.net/2Gwau7TtiHM5PdsjFeaxnm-320-80.jpg"
-      alt="Red Panda"
-    />
+    <img className={classes.image} src={props.src} alt={props.alt} />
+    <p className={classes.caption}>{props.children}</p>
   </div>
 );
 export default Image;
